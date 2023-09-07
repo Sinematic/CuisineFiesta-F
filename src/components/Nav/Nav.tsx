d6e597a38deca888609b9aebbcecadfffcd15c7a
Maxime <maxime.rache@gmail.com>
2023-09-07 18:34:55 +0200

    Correction d'ajout des boutons de la nav et tests stroke-width

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,6 +2,10 @@ import { useState } from "react"
 import "../../styles/Nav/Nav.css"
 import DropdownMenu from "../Dropdown/DropdownMenu"
 import { useNavigate } from "react-router"
+import Home from "../../assets/icons/home.svg"
+import BurgerMenu from "../../assets/icons/burger-menu.svg"
+import Search from "../../assets/icons/search.svg"
+import User from "../../assets/icons/user.svg"
 
 function Nav() {
 
@@ -38,33 +42,21 @@ function Nav() {
     return (
         <>  
             <nav className="nav-bar">
+                <div onClick={() => navigate("/")} className="nav-icon home">
+                        <img src={Home} alt="Accueil" />                   
+                </div>
 
-                <div onClick={() => setisOpen(!isOpen)} className="nav-icon">
-
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" 
-                    strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                        <line x1="4" x2="20" y1="12" y2="12"/>
-                        <line x1="4" x2="20" y1="6" y2="6"/>
-                        <line x1="4" x2="20" y1="18" y2="18"/>
-                    </svg> 
-                                        
+                <div onClick={() => setisOpen(!isOpen)} className="nav-icon burger-menu">
+                    <img src={BurgerMenu} alt="Menu" />                   
                     <DropdownMenu isOpen={isOpen} links={hamburgerMenu} />
-
                 </div>
 
-                <div className="nav-icon" onClick={() => handleNavigation('/search')}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" fill="none" stroke="currentColor" strokeWidth="7">
-                        <circle cx="40" cy="40" r="30" />
-                        <line x1="65" y1="65" x2="90" y2="90" />
-                    </svg>
+                <div className="nav-icon search" onClick={() => handleNavigation('/search')}>
+                    <img src={Search} alt="Rechercher une recette" />
                 </div>
 
-                <div className="nav-icon" onClick={() => handleNavigation("/profile")}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" 
-                    strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                        <circle cx="12" cy="8" r="5"/>
-                        <path d="M20 21a8 8 0 1 0-16 0"/>
-                    </svg>
+                <div className="nav-icon user-profile" onClick={() => handleNavigation("/profile")}>
+                    <img src={User} alt="Vers la page de profil" />                   
                 </div>
             </nav>  
         </>
