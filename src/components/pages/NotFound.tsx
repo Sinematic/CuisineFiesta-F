50241eeffcf53352bf24e9e7bc48041dd2f80c42
Maxime <maxime.rache@gmail.com>
2023-10-18 11:09:51 +0200

    ajout redirection automatique de la 404

--- src/components/pages/NotFound.tsx
+++ src/components/pages/NotFound.tsx
@@ -1,13 +1,21 @@
+import { useNavigate } from "react-router"
 import "../../styles/pages/NotFound.css"
 import Nav from "../Nav/Nav"
 
 function NotFound() {
+
+    const navigate = useNavigate()
+
+    setTimeout(() => {
+        navigate("/")
+    }, 5000)
 
     return (
         <>
             <div className="wrapper-404">
                 <h1>Page non trouvée ! 🤯</h1>
                 <p>Tu as dû t'égarer, cette page n'existe pas.</p>
+                <p onClick={() => navigate("/")} className="redirect-home">Retour à la page d'accueil</p>
             </div>
             <Nav />
         </>
